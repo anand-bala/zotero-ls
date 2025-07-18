e9bc1b499ba7a804f71154ead28314ccac76bb31
Anand Balakrishnan <anandbala1597@gmail.com>
2025-07-18 16:06:45 -0700

    feat: query BetterBibTeX JSON-RPC API for ready and exporting

---
+++ src/zotero_ls/bbt.py
@@ -0,0 +1,91 @@
+"""Fetch information from a running intance of Better BibTeX"""
+
+import asyncio
+import enum
+from dataclasses import dataclass, field
+
+import aiohttp
+from pydantic import BaseModel, JsonValue, TypeAdapter
+
+from zotero_ls.json_rpc import JsonRpcClient
+
+
+class Translators(enum.StrEnum):
+    BIBTEX = "Better BibTeX"
+    BIBLATEX = "Better BibLaTeX"
+    BIBTEX_CITATION_KEY = "Better BibTeX Citation Key Quick Copy"
+    CSL_JSON = "Better CSL JSON"
+    CSL_YAML = "Better CSL YAML"
+    JSON = "BetterBibTeX JSON"
+
+
+class ReadyResponse(BaseModel):
+    """The Zotero and BetterBibTeX version to show the JSON-RPC API is ready."""
+
+    betterbibtex: str
+    zotero: str
+
+
+class ScanAuxResponse(BaseModel): ...
+
+
+ExportItemsAdapter = TypeAdapter(tuple[list[str], Translators, None | str] | tuple[list[str], Translators])
+
+
+@dataclass
+class Client:
+    """Client for the BetterBibTeX JSON-RPC API"""
+
+    rpc: JsonRpcClient = field(
+        default_factory=lambda: JsonRpcClient(
+            aiohttp.ClientSession(
+                base_url="http://127.0.0.1:23119/",
+            )
+        )
+    )
+
+    async def _send(self, method: str, data: None | list[JsonValue] = None) -> JsonValue | None:
+        return await self.rpc.send_request("/better-bibtex/json-rpc", method=method, data=data)
+
+    async def is_ready(self) -> ReadyResponse:
+        response = await self._send("api.ready", None)
+        return ReadyResponse.model_validate(response)
+
+    async def export_items(self, citekeys: list[str], translator: Translators, library_id: str | None = None) -> str:
+        if library_id is not None:
+            data = ExportItemsAdapter.dump_python((citekeys, translator, library_id))
+        else:
+            data = ExportItemsAdapter.dump_python((citekeys, translator))
+        response = await self._send("item.export", data)
+        assert isinstance(response, str)
+        return response
+
+    async def close(self) -> None:
+        await self.rpc.close()
+
+
+if __name__ == "__main__":
+    import atexit
+
+    loop = asyncio.new_event_loop()
+
+    async def start() -> Client:
+        print("starting TCP connection")
+        return Client()
+
+    client = loop.run_until_complete(start())
+
+    @atexit.register
+    def close_client() -> None:
+        loop.run_until_complete(client.close())
+        loop.close()
+
+    async def main() -> None:
+        rpc = client.rpc
+        print(f"{rpc.closed=}")
+        resp = await client.is_ready()
+        print(resp)
+        resp = await client.export_items(["balakrishnan2025monitoring"], Translators.BIBLATEX)
+        print(resp)
+
+    loop.run_until_complete(main())
